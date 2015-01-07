ebae61c7c8f057295ce6fba0743a96504b9e9032
Louis Roy <louis.roy@cossette.com>
2015-01-07 16:18:46 -0500

    remove console logs

--- js/app.js
+++ js/app.js
@@ -4,10 +4,9 @@ var $nav = $('.nav-header');
 $window.on('scroll', function(ev) {
 	var windowHeight = $window.height();
 	var percent = $window.scrollTop() / (windowHeight - $nav.height());
-	console.log(percent);
+	
 	if (percent >=1) {
 		$nav.addClass('nav-sticky');
-		
 	} else {
 		$nav.removeClass('nav-sticky');
 	}
